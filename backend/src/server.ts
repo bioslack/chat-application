1e6188f69b1a30955ce86610f57e217f7274eddc
Luis Pereira <pereira.luishm@gmail.com>
2023-02-16 00:03:47 -0300

    setup cors policy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,42 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import authRouter from './routes/auth';
 import userRouter from './routes/user';
 import messagesRouter from './routes/messages';
 import passport from './middlewares/passport';
 import expressSession from 'express-session';
+import cors, { CorsOptions } from 'cors';
 import http from 'http';
 
 const server = express();
 const app = http.createServer(server);
 
+const allowed = [
+  'http://localhost',
+  'https://localhost',
+  'http://localhost:3000',
+];
+
+const credentials = (req: Request, res: Response, next: NextFunction) => {
+  const origin = req.headers.origin;
+  if (allowed.includes(origin || 'unknown origing')) {
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  next();
+};
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (allowed.indexOf(origin || 'unknown origin') !== -1 || !origin)
+      return callback(null, true);
+    return callback(new Error('Restricted by CORS policy'));
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
 server.use(express.json());
+server.use(credentials);
+server.use(cors(corsOptions));
 server.use(
   expressSession({
     secret: process.env.EXPRESS_SESSION_SECRET || 'testing',
