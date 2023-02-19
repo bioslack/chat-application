307d940ef5d9711b436eaa1c4da39d33ab16b972
Luis Pereira <pereira.luishm@gmail.com>
2023-02-18 21:26:10 -0300

    fix save name issue during signup

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import passport from 'passport';
+import { prisma } from '../../prisma';
 
 const authRouter = Router();
 
 authRouter.post('/signup', async (req, res, next) => {
-  passport.authenticate('signup', (err, user, info) => {
+  passport.authenticate('signup', async (err, user, info) => {
     if (err) {
       console.log(err);
     }
@@ -16,6 +17,11 @@ authRouter.post('/signup', async (req, res, next) => {
       if (err) {
         return next(err);
       }
+    });
+
+    await prisma.user.update({
+      data: { name: req.body.name },
+      where: { id: user.id },
     });
 
     delete user.password;
